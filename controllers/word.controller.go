40591a63c0bea996f7d9b60bc3ba7c8816b6420d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-09-18 22:47:54 +0300

    implement word controller

---
+++ controllers/word.controller.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/AkifhanIlgaz/dictionary-api/services"
+	"github.com/AkifhanIlgaz/dictionary-api/utils/api"
+	"github.com/gin-gonic/gin"
+)
+
+type WordController struct {
+	wordService services.WordService
+}
+
+func NewWordController(wordService services.WordService) WordController {
+	return WordController{
+		wordService: wordService,
+	}
+}
+
+// TODO: Id ve index için aynı controller'ı kullan
+// TODO: Postman ile test et
+// TODO: Response functions
+func (controller WordController) GetWord(ctx *gin.Context) {
+
+	searchBy := ctx.Query(api.SearchByQueryParam)
+	value := ctx.Query(api.ValueQueryParam)
+	switch searchBy {
+	case api.SearchById:
+		word, err := controller.wordService.GetById(value)
+		if err != nil {
+			fmt.Println(err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+		}
+		ctx.JSON(http.StatusOK, &word)
+
+	case api.SearchByIndex:
+		index, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println(err)
+			ctx.AbortWithError(http.StatusBadRequest, err)
+		}
+
+		word, err := controller.wordService.GetByIndex(index)
+		if err != nil {
+			fmt.Println(err)
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+		}
+		ctx.JSON(http.StatusOK, &word)
+	default:
+		// TODO: Error constant
+		ctx.AbortWithError(http.StatusInternalServerError, fmt.Errorf("unsupported search value"))
+	}
+
+}
---
+++ utils/api/api.go
@@ -0,0 +1,11 @@
+package api
+
+const (
+	SearchByQueryParam string = "searchBy"
+	ValueQueryParam    string = "value"
+)
+
+const (
+	SearchById    string = "id"
+	SearchByIndex string = "index"
+)
