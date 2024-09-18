30c7c2c78fb94344b4520ba9aeb460186b897d33
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-09-18 22:14:27 +0300

    add word model

--- models/word.model.go
+++ models/word.model.go
@@ -1,3 +1,29 @@
 package models
 
+type WordInfo struct {
+	Index  int    `json:"index"`
+	Source string `json:"source"`
+	Word   string `json:"word"`
+	Header
+	Definitions []Definition `json:"definitions"`
+	Idioms      []Idiom      `json:"idioms"`
+}
 
+type Header struct {
+	Audio struct {
+		UK string `json:"UK" bson:"UK"`
+		US string `json:"US" bson:"US"`
+	} `json:"audio" bson:"audio"`
+	PartOfSpeech string `json:"partOfSpeech" bson:"partOfSpeech"`
+	CEFRLevel    string `json:"CEFRLevel" bson:"CEFRLevel"`
+}
+
+type Definition struct {
+	Meaning  string   `json:"meaning"`
+	Examples []string `json:"examples"`
+}
+
+type Idiom struct {
+	Usage       string       `json:"usage"`
+	Definitions []Definition `json:"definition"`
+}
